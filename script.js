0f0a7a48a407450f117e54e561deeedc7e71647a
MatveyZZ <matveyka.sokolov.2004@mail.ru>
2025-04-24 11:50:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const dergee = 6;
+const hr = document.querySelector('#hr');
+const min = document.querySelector('#min');
+const sec = document.querySelector('#sec');
+
+setInterval(() => {
+
+    const date = new Date();
+    const hh = date.getHours() * 30;
+    const mm = date.getMinutes() * dergee;
+    const ss = date.getSeconds() * dergee;
+
+    hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+    min.style.transform = `rotateZ(${mm}deg)`;
+    sec.style.transform = `rotateZ(${ss}deg)`;
+
+});
